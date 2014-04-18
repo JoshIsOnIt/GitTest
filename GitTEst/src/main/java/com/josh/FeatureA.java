dfbafeeaecaa198c10d8355fb4a7435f2e783ef7
Josh Blinick <jblinick@blinco.com>
2014-04-17 20:39:56 -0400

    cleaned whitespace

--- GitTEst/src/main/java/com/josh/FeatureA.java
+++ GitTEst/src/main/java/com/josh/FeatureA.java
@@ -6,18 +6,20 @@ public class FeatureA {
 
 	String featureName;
 	Date dateCreated;
+
 	public String getFeatureName() {
 		return featureName;
 	}
+
 	public void setFeatureName(String featureName) {
 		this.featureName = featureName;
 	}
+
 	public Date getDateCreated() {
 		return dateCreated;
 	}
+
 	public void setDateCreated(Date dateCreated) {
 		this.dateCreated = dateCreated;
 	}
-	
-	
 }
