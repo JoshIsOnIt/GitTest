42bc9df310403a9ce64172fdfdbf5de9a29894b2
Josh Blinick <jblinick@blinco.com>
2014-04-17 20:53:21 -0400

    Added age

--- GitTEst/src/main/java/com/josh/FeatureA.java
+++ GitTEst/src/main/java/com/josh/FeatureA.java
@@ -6,6 +6,15 @@ public class FeatureA {
 
 	String featureName;
 	Date dateCreated;
+	Integer age;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public String getFeatureName() {
 		return featureName;
