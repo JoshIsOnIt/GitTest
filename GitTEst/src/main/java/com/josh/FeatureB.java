a8f0bb7f0a9f0579eb14bc7bca669b72421e4087
Josh Blinick <jblinick@blinco.com>
2014-04-17 20:52:44 -0400

    Added Variables for it

--- GitTEst/src/main/java/com/josh/FeatureB.java
+++ GitTEst/src/main/java/com/josh/FeatureB.java
@@ -2,4 +2,23 @@ package com.josh;
 
 public class FeatureB {
 
+	String vara;
+	String varb;
+
+	public String getVara() {
+		return vara;
+	}
+
+	public void setVara(String vara) {
+		this.vara = vara;
+	}
+
+	public String getVarb() {
+		return varb;
+	}
+
+	public void setVarb(String varb) {
+		this.varb = varb;
+	}
+
 }
