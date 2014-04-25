d44d1218211611e882ecccdf299b62a10937362f
Josh Blinick <jblinick@blinco.com>
2014-04-25 14:53:04 -0400

    created hashCode(), equals()

--- GitTEst/src/main/java/com/josh/FeatureB.java
+++ GitTEst/src/main/java/com/josh/FeatureB.java
@@ -11,6 +11,37 @@ public class FeatureB {
 	String vara;
 	String varb;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((vara == null) ? 0 : vara.hashCode());
+		result = prime * result + ((varb == null) ? 0 : varb.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FeatureB other = (FeatureB) obj;
+		if (vara == null) {
+			if (other.vara != null)
+				return false;
+		} else if (!vara.equals(other.vara))
+			return false;
+		if (varb == null) {
+			if (other.varb != null)
+				return false;
+		} else if (!varb.equals(other.varb))
+			return false;
+		return true;
+	}
+
 	public String getVara() {
 		return vara;
 	}
