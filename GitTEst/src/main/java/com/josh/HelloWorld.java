cee4cb7ba4762ab8f9558694a9ea7a33ccbbda7b
Josh Blinick <jblinick@blinco.com>
2014-04-25 11:32:50 -0400

    Updated toString

--- GitTEst/src/main/java/com/josh/HelloWorld.java
+++ GitTEst/src/main/java/com/josh/HelloWorld.java
@@ -18,7 +18,7 @@ public class HelloWorld {
 	 */
 	@Override
 	public String toString() {
-		return var1 + var2 + var3;
+		return var1 + var2 + var3 + varForFeatureX;
 	}
 	
 	public String getVarForFeatureX() {
