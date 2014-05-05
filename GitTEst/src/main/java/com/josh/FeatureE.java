31a8068d649653ac1852eb89dca079af3b2564df
Josh Blinick <jblinick@blinco.com>
2014-05-05 14:19:28 -0400

    Added Feature E Properties

--- GitTEst/src/main/java/com/josh/FeatureE.java
+++ GitTEst/src/main/java/com/josh/FeatureE.java
@@ -2,9 +2,28 @@ package com.josh;
 
 /**
  * Feature E is a great feature
+ * 
  * @author jblinick
- *
+ * 
  */
 public class FeatureE {
 
+	String propertyA;
+	String propertyB;
+
+	public String getPropertyA() {
+		return propertyA;
+	}
+
+	public void setPropertyA(String propertyA) {
+		this.propertyA = propertyA;
+	}
+
+	public String getPropertyB() {
+		return propertyB;
+	}
+
+	public void setPropertyB(String propertyB) {
+		this.propertyB = propertyB;
+	}
 }
