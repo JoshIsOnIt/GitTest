b453c00e18fea6b9cdfa81e7ad9e7dacb7b57ba4
Josh Blinick <jblinick@blinco.com>
2014-05-05 14:20:12 -0400

    Getters and setters on Feature D

--- GitTEst/src/main/java/com/josh/FeatureD.java
+++ GitTEst/src/main/java/com/josh/FeatureD.java
@@ -7,7 +7,25 @@ public class FeatureD {
 	String featureName;
 	Date dateCreated;
 	
+
 	public FeatureD() {
 
 	}
+	
+	public String getFeatureName() {
+		return featureName;
+	}
+
+	public void setFeatureName(String featureName) {
+		this.featureName = featureName;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
 }
