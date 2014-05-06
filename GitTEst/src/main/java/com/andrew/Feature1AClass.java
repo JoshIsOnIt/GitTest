abe83aa2e4726e660d2a860e3e0d96b37cf723a5
Andrew Yan <ayan@blinco.com>
2014-05-06 11:08:31 -0400

    added getters/setters (+3 squashed commit)
    Squashed commit:
    
    [343cf2c] added properties
    
    [c5de0d6] removed comments
    
    [6068320] added javadoc

--- GitTEst/src/main/java/com/andrew/Feature1AClass.java
+++ GitTEst/src/main/java/com/andrew/Feature1AClass.java
@@ -1,13 +1,29 @@
 package com.andrew;
 
-/** 
+/**
  * Javadoc for class
+ * 
  * @author ayan
- *
+ * 
  */
 public class Feature1AClass {
-	// something
-	
-	
-	// and more things
+
+	String propertyA;
+	String propertyB;
+
+	public String getPropertyA() {
+		return propertyA;
+	}
+
+	public void setPropertyA(String propertyA) {
+		this.propertyA = propertyA;
+	}
+
+	public String getPropertyB() {
+		return propertyB;
+	}
+
+	public void setPropertyB(String propertyB) {
+		this.propertyB = propertyB;
+	}
 }
